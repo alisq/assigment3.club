4edde37532fada8c165638946971e89a4cb7a17a
alisq <ali.shamas.qadeer@gmail.com>
2020-03-28 19:40:57 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,14 +3,12 @@ $(".close").click(function(){
 })
 
 var countDownDate = new Date("Mar 30, 2020 20:00:00").getTime();
+
+calculate();
+
+
 var x = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  $("#countdown").html(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+  calculate();
 
   // If the count down is finished, write some text
   if (distance < 0) {
@@ -18,3 +16,14 @@ var x = setInterval(function() {
     //$("#countdown").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+
+function calculate() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  $("#countdown").html(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+}
