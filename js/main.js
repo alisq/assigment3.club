780c0c3559a49344eade3f6232e7ed9357ead741
alisq <ali.shamas.qadeer@gmail.com>
2020-03-30 08:30:57 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ function calculate() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  $("#countdown").html(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+  $("#countdown").html(/*days + "d " +*/ hours + "h " + minutes + "m " + seconds + "s ");
 
 
   if (distance < 0) {
