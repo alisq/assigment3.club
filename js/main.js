9a5b02b62029cb247267640f4cc76923a1278240
alisq <ali.shamas.qadeer@gmail.com>
2020-03-30 08:18:30 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(".close").click(function(){
   $("#countdown-container").fadeOut(200)
 })
 
-var countDownDate = new Date("Mar 30, 2020 23:00:00").getTime();
+var countDownDate = new Date("Mar 30, 2020 20:30:00").getTime();
 
 calculate();
 
