502fa33d3a0a1344c8f19ad2c821dadf493c7821
alisq <ali.shamas.qadeer@gmail.com>
2020-03-30 21:58:10 -0400

    updated to include second lesson time

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(".close").click(function(){
   $("#countdown-container").fadeOut(200)
 })
 
-var countDownDate = new Date("Mar 30, 2020 20:30:00").getTime();
+var countDownDate = new Date("Apr 1, 2020 20:30:00").getTime();
 
 calculate();
 
